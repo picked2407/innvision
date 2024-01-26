eb5f43a23b14a8e1f6c0bb54fe2433675ba3fc76
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-26 18:22:58 +0530

    Update Blog.tsx

--- src/app/components/Blog.tsx
+++ src/app/components/Blog.tsx
@@ -1,21 +1,8 @@
-import BlogCard from "./BlogCard";
-
-const Blog = () => {
+const HowitWorks = () => {
     return (
-        <section id="blog">
-            <div className="container py-20">
-                <div className="text-center m-auto mb-20 md:w-1/2">
-                    <h4 className="font-bold text-color-secondary mb-4">
-                        Innovation And Quality Improvement
-                    </h4>
-                    <h1 className="title">
-                        Latest Updates, Solutions And Company News
-                    </h1>
-                </div>
-                <BlogCard />
-            </div>
-        </section>
+        // Placeholder expression
+        <div>        </div>
     );
 };
 
-export default Blog;
+export default HowitWorks;
