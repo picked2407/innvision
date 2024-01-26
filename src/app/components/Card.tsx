48bf8d23ffad6f9956d2582c4f962af7af8491b6
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-26 18:23:51 +0530

    Update Card.tsx

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -11,9 +11,9 @@ const Card = () => {
                         <path d="M0 464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V192H0v272zm320-196c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM192 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12h-40c-6.6 0-12-5.4-12-12v-40zM64 268c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zm0 128c0-6.6 5.4-12 12-12h40c6.6 0 12 5.4 12 12v40c0 6.6-5.4 12-12 12H76c-6.6 0-12-5.4-12-12v-40zM400 64h-48V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H160V16c0-8.8-7.2-16-16-16h-32c-8.8 0-16 7.2-16 16v48H48C21.5 64 0 85.5 0 112v48h448v-48c0-26.5-21.5-48-48-48z" />
                     </svg>
                 </div>
-                <h3 className="text-xl font-bold py-4">Expense Trackering</h3>
+                <h3 className="text-xl font-bold py-4">On-Time Delivery and Regular Updates</h3>
                 <p className="leading-relaxed">
-                    We use an application designed a testing gnose to keep away
+                InnVision guarantees timely delivery and keeps you in the loop with regular updates, ensuring your website is not just built efficiently but evolves collaboratively.
                 </p>
             </div>
             <div className="bg-color-primary-dark border-2 border-solid border-color-gray text-center py-20 px-5 rounded-2xl cursor-pointer hover:bg-color-primary-dark ease-in duration-200">
@@ -26,9 +26,9 @@ const Card = () => {
                         <path d="M332.8 320h38.4c6.4 0 12.8-6.4 12.8-12.8V172.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v134.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V76.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v230.4c0 6.4 6.4 12.8 12.8 12.8zm-288 0h38.4c6.4 0 12.8-6.4 12.8-12.8v-70.4c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v70.4c0 6.4 6.4 12.8 12.8 12.8zm96 0h38.4c6.4 0 12.8-6.4 12.8-12.8V108.8c0-6.4-6.4-12.8-12.8-12.8h-38.4c-6.4 0-12.8 6.4-12.8 12.8v198.4c0 6.4 6.4 12.8 12.8 12.8zM496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16z" />
                     </svg>
                 </div>
-                <h3 className="text-xl font-bold py-4">Finance Snapshot</h3>
+                <h3 className="text-xl font-bold py-4">Low-Cost Websites with High-Quality Work</h3>
                 <p className="leading-relaxed">
-                    We use an application designed a testing gnose to keep away
+                At InnVision, we provide cost-effective websites without compromising quality, delivering excellence in web development that fits your budget.
                 </p>
             </div>
             <div className="border-2 border-solid border-color-gray text-center py-20 px-5 rounded-2xl cursor-pointer hover:bg-color-primary-dark ease-in duration-200">
@@ -41,9 +41,9 @@ const Card = () => {
                         <path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
                     </svg>
                 </div>
-                <h3 className="text-xl font-bold py-4">Support 24/24</h3>
+                <h3 className="text-xl font-bold py-4">High-Quality Work at Affordable Rates</h3>
                 <p className="leading-relaxed">
-                    We use an application designed a testing gnose to keep away
+                InnVision: Elevating Excellence, Affordable Web Solutions, Premium Development.
                 </p>
             </div>
         </div>
