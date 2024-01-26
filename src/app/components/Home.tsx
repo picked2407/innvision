e28aa8dca8f3e3552009b34566cb7c1a60177339
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-26 18:25:38 +0530

    Update Home.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -10,14 +10,12 @@ const Home = () => {
                 <div className="flex flex-col items-center z-20 md:flex-row">
                     <div className="text-center mb-12 md:text-left md:w-1/2 md:pr-10">
                         <h1 className="title">
-                            Awesome App for your Financial
+                        InnVision: Illuminating Digital Brilliance, Crafting Tomorrow's Web Today.
                         </h1>
                         <p className="leading-relaxed mb-10">
-                            This should be used to tell a story and let your
-                            users know a little more about app and it's use, How
-                            can benefit them.
+                        We're more than a web development agency – we're innovators, creators, and your digital partner. Our passionate team brings your brand to life with cutting-edge tech and sleek design. Whether it's a modern website, e-commerce platform, or custom solution, we're here for you. Join us in building a captivating digital presence that propels your brand forward. InnVision - Where Possibilities Shine, Excellence Defined.
                         </p>
-                        <button className="btn">Download App</button>
+                        <button className="btn"><a href="#features">Get started</a></button>
                     </div>
                     <div className="md:w-1/2 flex justify-center">
                         <Image
