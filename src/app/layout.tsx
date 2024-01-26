8c7d737f17e1d835b75a24842d0581bbd090f30f
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-01-26 19:04:41 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import NavBar from "./components/Shared/NavBar";
 import "./globals.css";
 
 export const metadata = {
-  title: "Next.js 14",
-  description: "A Next.js starter with Tailwind CSS and TypeScript",
+  title: "Innvision",
+  description: "We're more than a web development agency – we're innovators, creators, and your digital partner. Our passionate team brings your brand to life with cutting-edge tech and sleek design. Whether it's a modern website, e-commerce platform, or custom solution, we're here for you. Join us in building a captivating digital presence that propels your brand forward. InnVision - Where Possibilities Shine, Excellence Defined.",
 };
 
 export default function RootLayout({
